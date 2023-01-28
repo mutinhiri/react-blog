6f742ad9f4f0e31523d6fc99682887c08753bebc
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-28 14:57:25 +0200

    Invoke handleClick

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return ( 
         <div className="home">
             <h2>Home here </h2>
-            <button>Click me </button>
+            <button onClick={ handleClick() }>Click me </button>
         </div>
      );
 }
