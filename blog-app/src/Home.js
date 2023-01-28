681fce4e06b095ad19c3402386c199177def3f40
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-28 21:07:29 +0200

    Add default state

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -1,25 +1,10 @@
 import { useState } from "react"
 
 const Home = () => {
-// let name = 'John' 
-    const [name, setName] = useState('John')
-    const [age, setAge] = useState(25)
 
-    const handleClick = (e) => {
-        setName('Paul')
-        setAge('50')
-        console.log('hello click', e)
-    }
-
-    // const clickMe = (name, e) => {
-    //     console.log('hello ' + name, e.target)
-    // }
     return ( 
         <div className="home">
-            <h2>Home here </h2>
-            <button onClick={ handleClick }>Click me </button>
-            {/* <button onClick={ (e) => {clickMe('jospw', e) }}>Click again</button> */}
-            <p>{name}  is {age} years old</p>
+     
         </div>
      );
 }
