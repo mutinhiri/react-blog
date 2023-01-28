d2c4a66c00c545f5370a3cac99119f75c0b73bed
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-28 20:51:14 +0200

    Add state var

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -1,7 +1,8 @@
 import { useState } from "react"
 
 const Home = () => {
-let name = 'John' 
+// let name = 'John' 
+    const [name, setName] = useState('John')
     const handleClick = (e) => {
         console.log('hello click', e)
     }
