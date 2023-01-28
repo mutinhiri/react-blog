be2d0b2dffb595959850de6f31efbc6674d9dd81
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-28 15:07:29 +0200

    Add wrap up

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -3,10 +3,15 @@ const Home = () => {
     const handleClick = () => {
         console.log('hello click')
     }
+
+    const clickMe = (name) => {
+        console.log('hello ' + name)
+    }
     return ( 
         <div className="home">
             <h2>Home here </h2>
             <button onClick={ handleClick }>Click me </button>
+            <button onClick={ () => {clickMe('jospw') }}>Click again</button>
         </div>
      );
 }
