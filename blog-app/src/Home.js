514aeb621320908d43f23b0814d130d3b7a1006e
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-28 20:54:34 +0200

    Add age

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -3,6 +3,8 @@ import { useState } from "react"
 const Home = () => {
 // let name = 'John' 
     const [name, setName] = useState('John')
+    const [age, setAge] = useState(25)
+
     const handleClick = (e) => {
         setName('Paul')
         console.log('hello click', e)
