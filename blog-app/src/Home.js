e936660ad6891631dcaf54c1a1452b25c83e03ac
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-28 21:37:16 +0200

    Add fil;e dependency

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -7,13 +7,7 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }])
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2> {blog.title}</h2>
-                    <p>Written by: {blog.author}</p>
-
-                </div>
-            ))}
+        
      
         </div>
      );
