ecd86a3a842c8dec52a73389ed82a1988d1a4982
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-29 11:56:42 +0200

    Render blogs after add

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -8,7 +8,8 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }])
 
     const handleDelete = (id) => {
-        const newBlogs = blogs.filter()
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
     }
     return ( 
         <div className="home">
