5e50a520b02fc3e1fc118f88f48512a2aae90245
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-29 21:43:32 +0200

    Add exact path

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Navbar/>
         <div className="content">
         <Switch>
-          <Route path='/'>
+          <Route exact path='/'>
              <Home/>
           </Route>
           <Route path='/create'>
