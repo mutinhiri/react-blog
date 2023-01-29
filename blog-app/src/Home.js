85fae0ddc191448b2fe40a0e36eeec71bd5acb92
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-29 19:59:32 +0200

    Add save error

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
             setIsPending(false)
         })
         .catch(err => {
-            console.log(err)
+            console.log(err.message)
         })
     }, [])
     return ( 
