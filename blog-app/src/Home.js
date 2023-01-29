8f9a6c5ef0027aa2db2123d38d2a1661d0a343f0
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-29 17:28:05 +0200

    Add from state

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -2,10 +2,7 @@ import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([  
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }])
+    const [blogs, setBlogs] = useState(null)
 
     const title = 'new title set now'
 
