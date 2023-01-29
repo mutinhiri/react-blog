073c6752987d8c6ce8706caecfee98d7aa07db74
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-29 11:51:21 +0200

    Add delete implementation

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -6,9 +6,13 @@ const Home = () => {
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }])
+
+    const handleDelete = () => {
+        
+    }
     return ( 
         <div className="home">
-         <BlogList blogs={blogs} title="All Bloggers"/>
+         <BlogList blogs={blogs} title="All Bloggers" handleDelete={handleDelete}/>
          <BlogList blogs={blogs.filter((blog) => {
            return  blog.author === 'mario'
          })} title="Mario's blogs"/>
