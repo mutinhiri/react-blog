791dfcfddacf13e0874d0f8130682e27c02841a7
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-29 17:40:24 +0200

    Remopve delete

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -6,10 +6,10 @@ const Home = () => {
 
     const title = 'new title set now'
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id !== id)
+    //     setBlogs(newBlogs)
+    // }
 
     useEffect(() => {
       fetch(' http://localhost:8000/blogs')
@@ -23,7 +23,7 @@ const Home = () => {
     }, [])
     return ( 
         <div className="home">
-        {blogs && <BlogList blogs={blogs} title="All Bloggers" handleDelete={handleDelete}/>}
+        {blogs && <BlogList blogs={blogs} title="All Bloggers" />}
          {/* <BlogList blogs={blogs.filter((blog) => {
            return  blog.author === 'mario'
          })} title= {title} handleDelete={handleDelete}/> */}
