fc884f8d517e659a93d954d2e3d75622b7dd52a9
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-29 12:01:24 +0200

    Add code cleanup

--- blog-app/src/BlogList.js
+++ blog-app/src/BlogList.js
@@ -1,6 +1,5 @@
 const BlogList = ({blogs, title, handleDelete}) => {
-    // const blogs= props.blogs
-    // const title = props.title
+    
     return ( 
         <div className="blog-list">
             <h2> {title} </h2>
