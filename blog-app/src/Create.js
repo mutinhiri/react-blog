98f80adbfab81ccfed0cbb566415af004f683428
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-30 21:11:23 +0200

    Check render side

--- blog-app/src/Create.js
+++ blog-app/src/Create.js
@@ -5,6 +5,7 @@ const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
+
     return ( 
         <div className="create">
             <h2> Add a new blog</h2>
@@ -23,7 +24,8 @@ const Create = () => {
 
               </textarea>
                 <label>Blog author</label>
-                <select >
+                <select value={author}
+                    onChange={(e) =>{setAuthor(e.target.value)}}>
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
