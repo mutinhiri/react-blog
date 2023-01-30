8daa7e8ace4998ef0c87f207f8796c3a360177f9
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-30 08:35:02 +0200

    UseFetch use hook

--- blog-app/src/BlogDetails.js
+++ blog-app/src/BlogDetails.js
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
 const BlogDetails = () => {
-const { id } = useParams()
+    const { id } = useParams()
+    const { data:blog, isPending } = useFetch('http://localhost:8000/blogs/' + id)
    
     return ( 
         <div className="blog-details">
