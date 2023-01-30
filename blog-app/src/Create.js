733f3bc1ff9acca3a77518bd1197d283be60d27b
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-30 20:41:04 +0200

    Add new form

--- blog-app/src/Create.js
+++ blog-app/src/Create.js
@@ -2,6 +2,9 @@ const Create = () => {
     return ( 
         <div className="title">
             <h2> Add a new blog</h2>
+            <form>
+                
+            </form>
         </div>
      );
 }
