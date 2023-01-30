bc0388a6f90b391db72af5e7b26e5fd8dc1b3d74
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-30 08:49:05 +0200

    Add error render

--- blog-app/src/BlogDetails.js
+++ blog-app/src/BlogDetails.js
@@ -3,12 +3,13 @@ import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams()
-    const { data:blog, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+    const { data:blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id)
    
     return ( 
         <div className="blog-details">
             {/* <h2> Blog details- { id }</h2> */}
             { isPending && <div> Loading ...</div>}
+            { error && <div> { error }</div>}
         </div>
      );
 }
