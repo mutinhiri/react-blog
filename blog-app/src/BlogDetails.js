337daf3ae5bf12cf52d43ed945c901398613f092
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-31 22:50:46 +0200

    Add render to redirect

--- blog-app/src/BlogDetails.js
+++ blog-app/src/BlogDetails.js
@@ -1,14 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams()
     const { data:blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id)
+    const history = useHistory()
 
     const handleDelete = () => {
         fetch('http://localhost:8000/blogs/'+ blog.id), {
             method: 'DELETE'
-        }
+        }.then(() => {
+
+        })
     }
    
     return ( 
