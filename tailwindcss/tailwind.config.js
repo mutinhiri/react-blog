dfe181c126584efc2200f6e698a98739bb91ac5b
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-02-06 09:07:28 +0200

    Add changes

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,6 +1,14 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
